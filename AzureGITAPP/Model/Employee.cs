b85a4dfd49e57e2793686a0a5dc2333ce1cec492
Gagan Gautam <gaganIT1265@outlook.com>
2022-11-01 17:29:45 +0530

    Added staic code for table data

---
+++ AzureGITAPP/Model/Employee.cs
@@ -0,0 +1,14 @@
+﻿namespace AzureGITAPP.Model
+{
+    public class Employee
+    {
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Department { get; set; }
+
+        public int Salary { get; set; }
+    }
+}
--- AzureGITAPP/Pages/Privacy.cshtml.cs
+++ AzureGITAPP/Pages/Privacy.cshtml.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AzureGITAPP.Model;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace AzureGITAPP.Pages
@@ -12,8 +13,31 @@ namespace AzureGITAPP.Pages
             _logger = logger;
         }
 
+        public List<Employee> employeesList = new List<Employee>();
+        public List<Employee> GetData()
+        {
+            List<Employee> employees = new List<Employee>() {
+                new Employee(){
+                    Id = 1,
+                    Name="Employee 1",
+                    Department ="Admin",
+                    Salary=15000,
+                },
+                 new Employee(){
+                    Id = 2,
+                    Name="Employee 2",
+                    Department ="Admin",
+                    Salary=18000,
+                },
+            };
+
+            return employees;
+        }
+
         public void OnGet()
         {
+            employeesList = GetData();
+
         }
     }
 }
